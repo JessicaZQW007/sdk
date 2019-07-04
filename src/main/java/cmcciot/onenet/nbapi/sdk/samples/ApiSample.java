2a337ee7588d40c00f891aa32245c964e042ae23
10713 <1071306022@qq.com>
2019-07-04 13:35:30 +0800

    测试

--- src/main/java/cmcciot/onenet/nbapi/sdk/samples/ApiSample.java
+++ src/main/java/cmcciot/onenet/nbapi/sdk/samples/ApiSample.java
@@ -58,7 +58,7 @@ public class ApiSample {
         ObserveOpe observeOpe = new ObserveOpe(apiKey);
         Observe observe = new Observe(imei, objId, false);
         LOGGER.info(observeOpe.operation(observe, null).toString());
-        System.out.println(" 上传测试");
+        System.out.println(" 上传测试4564654656");
 
     }
 }
